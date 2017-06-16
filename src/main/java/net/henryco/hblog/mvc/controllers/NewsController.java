6d26966ac1c0f22ec3c792d3374be8566bca62ef
HenryCo <hd2files@gmail.com>
2017-06-16 23:20:55 +0200

    Uni post dao added Repositories and dao created Post content and post preview separated Controller and templates update

--- src/main/java/net/henryco/hblog/mvc/controllers/NewsController.java
+++ src/main/java/net/henryco/hblog/mvc/controllers/NewsController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -19,7 +21,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping("/news")
 public class NewsController {
 
-	private static final long NEWS_ON_PAGE = 6;
+	private static final long NEWS_ON_PAGE = 6L;
+	private static final int LINK_PAGE_NUMB = 7;
 
 	private final NewsPageService newsPageService;
 
@@ -46,12 +49,41 @@ public class NewsController {
 		if (pageNumb <= 0) return news();
 		if (pageNumb > 1) model.addAttribute("pageNumber", pageNumb);
 
+		List<Long> linksArray = getLinksArray(LINK_PAGE_NUMB, pageNumb, maxPages);
+
 		List<StandardPostPreview> posts = newsPageService.getLastPostsInRange(pageNumb - 1, NEWS_ON_PAGE);
 		model.addAttribute("posts", posts);
 		model.addAttribute("page_prev", pageNumb - 1);
 		model.addAttribute("page_next", pageNumb + 1);
 		model.addAttribute("page_max", maxPages);
+		model.addAttribute("link_page_min", linksArray.get(0));
+		model.addAttribute("link_page_max", linksArray.get(linksArray.size() - 1));
+		model.addAttribute("link_array", linksArray);
 
 		return "news";
+	}
+
+	private static List<Long> getLinksArray(int size, long page, long max) {
+
+		List<Long> list = new ArrayList<>();
+		long start = page - ((size - 1) / 2);
+
+		if (start <= 0) {
+			for (long i = 1; i < size; i++) list.add(i);
+			return list;
+		}
+
+		long end = start + size;
+		if (end > max) {
+			for (long i = max - size; i <= max; i++) list.add(i);
+			return list;
+		}
+
+		for (int i = 0; i < size; i++) {
+			long p = start + i;
+			if (p >= 1 && p < max) list.add(p);
+		}
+
+		return list;
 	}
 }
